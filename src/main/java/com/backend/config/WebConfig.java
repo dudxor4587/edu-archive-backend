f1c372eedf6b3efc4c5ac707586dbf9ef2d35a4c
dudxor4587 <dudxor4587@gmail.com>
2024-11-25 18:38:31 +0000

    feat : CORS 설정 추가

---
+++ src/main/java/com/backend/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("https://edu-archive.site") // “*“같은 와일드카드를 사용
+                .allowedMethods("GET", "POST")
+                .allowCredentials(true); // 쿠키 인증 요청 허용
+    }
+}
